6f9bca6ce97c0425af4881fe9e23d40b485b6e28
achmadluthfan <achmadluthfannurirsyad@mail.ugm.ac.id>
2026-03-25 16:19:32 +0700

    learn babel parser

--- index.js
+++ index.js
@@ -6,57 +6,68 @@ import { allowedNodeEnvironmentFlags, argv } from "node:process";
 import path from "node:path";
 // read all file inside of directory, file, check is directory / file, open specific file
 import { readdir, readFile, stat } from "fs/promises";
+// babel Parser for Abstract Syntax Tree
+import parser from "@babel/parser";
 
-// use forEach because not return new array
-argv.forEach(async (val, index) => {
-  const isScan = val.includes("scan");
+// // use forEach because not return new array
+// argv.forEach(async (val, index) => {
+//   const isScan = val.includes("scan");
 
-  // ======= 📂 Phase 2 — File System Exploration =======
-  if (isScan) {
-    for (let i = index; i <= index; i++) {
-      // 1. resolve path from argument first
-      const targetPath = path.resolve(argv[i + 1]);
+//   // ======= 📂 Phase 2 — File System Exploration =======
+//   if (isScan) {
+//     for (let i = index; i <= index; i++) {
+//       // 1. resolve path from argument first
+//       const targetPath = path.resolve(argv[i + 1]);
 
-      // 2. check the arg is file or directory
-      const isFile = (await stat(targetPath)).isFile();
-      if (isFile) {
-        // ===== fileHandle approach (use open from fs) ===== -> "buka file dulu → pegang aksesnya → nanti gue baca sendiri"
-        // const fileHandle = await open(targetPath);
-        // console.log("fileHandle", fileHandle);
-        // const dataFromFileHandle = await fileHandle.readFile({
-        //   encoding: "utf-8",
-        // });
+//       // 2. check the arg is file or directory
+//       const isFile = (await stat(targetPath)).isFile();
+//       if (isFile) {
+//         // ===== fileHandle approach (use open from fs) ===== -> "buka file dulu → pegang aksesnya → nanti gue baca sendiri"
+//         // const fileHandle = await open(targetPath);
+//         // console.log("fileHandle", fileHandle);
+//         // const dataFromFileHandle = await fileHandle.readFile({
+//         //   encoding: "utf-8",
+//         // });
 
-        // ===== readFile approach (use readFile from fs) ==== -> "Bro, buka file ini → baca → kasih gue isinya"
-        const dataFile = await readFile(targetPath, "utf8");
-        if (dataFile.includes("console.log")) {
-          console.log("ada conggg");
-        } else {
-          console.log("hell nahhh broww");
-        }
-      } else {
-        // read all file inside of directory
-        const allFiles = await readdir(targetPath);
-        allFiles.shift();
-        console.log("allFiles after removed with pop", allFiles);
+//         // ===== readFile approach (use readFile from fs) ==== -> "Bro, buka file ini → baca → kasih gue isinya"
+//         const dataFile = await readFile(targetPath, "utf8");
+//         if (dataFile.includes("console.log")) {
+//           console.log("ada conggg");
+//         } else {
+//           console.log("hell nahhh broww");
+//         }
+//       } else {
+//         // read all file inside of directory
+//         const allFiles = await readdir(targetPath);
+//         allFiles.shift();
 
-        // use forEach because not need to return or edit the targetPath variable
-        allFiles.forEach(async (file) => {
-          const data = await readFile(file, "utf8");
-          if (data.includes("console.log")) {
-            console.log("ada cokkkk, ", `File : ${file}`);
-          } else {
-            console.log("hell nah");
-          }
-        });
-      }
+//         // use forEach because not need to return or edit the targetPath variable
+//         allFiles.forEach(async (file) => {
+//           const data = await readFile(file, "utf8");
+//           if (data.includes("console.log")) {
+//             console.log("ada cokkkk, ", `File : ${file}`);
+//           } else {
+//             console.log("hell nah");
+//           }
+//         });
+//       }
 
-      // if doenst -> use readdir to get information about file at the directory
+//       // if doenst -> use readdir to get information about file at the directory
 
-      //   argv[i + 1];
-    }
-  }
+//       //   argv[i + 1];
+//     }
+//   }
+// });
+
+const myFunction = "() => console.log(`test`)";
+
+// const myFunction = () => console.log(`test`);
+
+const ast = parser.parse(myFunction, {
+  sourceType: "module",
 });
+
+console.log("ast", ast.program.body);
 
 // 📂 Phase 2 — File System Exploration
 
